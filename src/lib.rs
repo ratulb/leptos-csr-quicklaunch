571a34dd415e7dae869d8a4d05f5437e4f25fa2b
Ratul Buragohain <ratul75@hotmail.com>
2024-02-20 21:09:39 +0000

    On-going changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-mod panic_hook;
-use wasm_bindgen::prelude::*;
+use wasm_bindgen::prelude::wasm_bindgen;
+use leptos::{IntoView, component, view, create_signal, mount_to_body,SignalUpdate, For, logging, SignalGet, SignalWith, create_memo}; 
 
 #[wasm_bindgen]
 extern "C" {
@@ -8,5 +8,65 @@ extern "C" {
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, {{project-name}}!");
+    alert("Hello, mushy-mushy!");
 }
+
+#[wasm_bindgen(start)]
+pub fn main() {
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+    mount_to_body(|| {
+        view! { <App/> }
+    })
+}
+
+#[derive(Debug, Clone)]
+struct DatabaseEntry {
+    key: String,
+    age: u8,
+}
+
+#[component]
+pub fn App() -> impl IntoView {
+    // start with a set of three rows
+    let (data, set_data) = create_signal(vec![
+        DatabaseEntry {
+            key: "foo".to_string(),
+            age: 10,
+        },
+        DatabaseEntry {
+            key: "bar".to_string(),
+            age: 20,
+        },
+        DatabaseEntry {
+            key: "baz".to_string(),
+            age: 15,
+        },
+    ]);
+    view! {
+        <button on:click=move |_| {
+            set_data
+                .update(|data| {
+                    for row in data {
+                        row.age *= 2;
+                    }
+                });
+            logging::log!("{:?}", data.get());
+        }>"Update Values"</button>
+        // iterate over the rows and display each age
+        <For
+            each=move || data.get().into_iter().enumerate()
+            key=|(_, state)| state.key.clone()
+            children=move |(index, _)| {
+                let age = create_memo(move |_| {
+                    data.with(|data| data.get(index).map(|d| d.age).unwrap_or(0))
+                });
+                view! {
+                    // <p>{move || age.get()}</p>
+                    <p>{age}</p>
+                }
+            }
+        />
+    }
+}
+
--- src/panic_hook.rs
+++
@@ -1,8 +0,0 @@
-/***#[allow(dead_code)]
-pub fn set_panic_hook() {
-    // We will get better error messages if our code ever panics.
-    //
-    // For more details see
-    // https://github.com/rustwasm/console_error_panic_hook#readme
-    console_error_panic_hook::set_once();
-}***/

e639196351f2e1375fb1adcfec0cb3e83058a0d8
Ratul Buragohain <ratul75@hotmail.com>
2024-02-20 01:05:00 +0000

    On-going changes

--- build.rs
+++ build.rs
@@ -1,23 +1,28 @@
 fn main() {
     use std::process::Command;
     println!("cargo:rerun-if-changed=build.tmp");
+    fn log(msg: String) {
+        if let Ok(_) = std::env::var("DEBUG") {
+            println!("{msg}");
+        }
+    }
     if cfg!(target_os = "linux") && cfg!(target_arch = "x86_64") {
         // Additional check for Arch Linux
         if std::fs::read_to_string("/etc/os-release")
             .map(|content| content.contains("Arch Linux"))
             .unwrap_or(false)
         {
-            println!("Running on Arch Linux");
+            log("Running on Arch Linux".to_string());
             check_install!("cargo make --version", "sudo pacman -S cargo-make");
         } else {
-            println!("Running on Linux (x86_64)");
+            log("Running on Linux (x86_64)".to_string());
             check_install!(
                 "cargo make --version",
                 "cargo install --no-default-features --force cargo-make"
             );
         }
     } else {
-        println!("Not running on Linux (x86_64)");
+        log("Not running on Linux (x86_64)".to_string());
         check_install!(
             "cargo make --version",
             "cargo install --no-default-features --force cargo-make"
@@ -34,22 +39,22 @@ macro_rules! check_install {
             Command::new("sh").arg("-c").arg(install).output()
         };
         match installed {
-            Ok(output) if output.stdout.len() > 0 => println!(
+            Ok(output) if output.stdout.len() > 0 => log(format!(
                 "[{}] Executed with resonse {:?}",
                 install,
                 std::str::from_utf8(&output.stdout)
-            ),
-            Ok(output) if !output.status.success() && output.stderr.len() > 0 => println!(
+            )),
+            Ok(output) if !output.status.success() && output.stderr.len() > 0 => log(format!(
                 "[{}] Executed with resonse code {:?} and message {:?} ",
                 install,
                 output.status,
                 std::str::from_utf8(&output.stderr)
-            ),
-            Ok(output) => eprintln!(
+            )),
+            Ok(output) => log(format!(
                 "Install command [{}] execution response {:?}",
                 install, output
-            ),
-            Err(err) => eprintln!("Error {:?} executing [{}]", err, install),
+            )),
+            Err(err) => log(format!("Error {:?} executing [{}]", err, install)),
         }
     }};
 
@@ -64,39 +69,39 @@ macro_rules! check_install {
 
         let present = match present {
             Ok(output) if output.stdout.len() > 0 => {
-                println!(
+                log(format!(
                     "[{}] Executed with resonse {:?}",
                     install,
                     std::str::from_utf8(&output.stdout)
-                );
+                ));
                 true
             }
 
             Ok(output) if !output.status.success() && output.stderr.len() > 0 => {
-                println!(
+                log(format!(
                     "[{}] Executed with resonse code {:?} and message {:?} ",
                     install,
                     output.status,
                     std::str::from_utf8(&output.stderr)
-                );
+                ));
                 false
             }
 
             Ok(output) => {
-                println!(
+                log(format!(
                     "Install command [{}] execution response {:?}",
                     install, output
-                );
+                ));
                 true
             }
             Err(err) => {
-                eprintln!("Error {:?} executing [{}]", err, install);
+                log(format!("Error {:?} executing [{}]", err, install));
                 false
             }
         };
 
         match present {
-            true => println!("[{}] is present", check),
+            true => log(format!("[{}] is present", check)),
             false => {
                 let installed = if cfg!(target_os = "windows") {
                     Command::new("cmd").args(["/C", install]).output()
@@ -104,23 +109,25 @@ macro_rules! check_install {
                     Command::new("sh").arg("-c").arg(install).output()
                 };
                 match installed {
-                    Ok(output) if output.stdout.len() > 0 => println!(
+                    Ok(output) if output.stdout.len() > 0 => log(format!(
                         "[{}] Executed with resonse {:?}",
                         install,
                         std::str::from_utf8(&output.stdout)
-                    ),
+                    )),
 
-                    Ok(output) if !output.status.success() && output.stderr.len() > 0 => println!(
-                        "[{}] Executed with resonse code {:?} and message {:?} ",
-                        install,
-                        output.status,
-                        std::str::from_utf8(&output.stderr)
-                    ),
-                    Ok(output) => eprintln!(
+                    Ok(output) if !output.status.success() && output.stderr.len() > 0 => {
+                        log(format!(
+                            "[{}] Executed with resonse code {:?} and message {:?} ",
+                            install,
+                            output.status,
+                            std::str::from_utf8(&output.stderr)
+                        ))
+                    }
+                    Ok(output) => log(format!(
                         "Install command [{}] execution response {:?}",
                         install, output
-                    ),
-                    Err(err) => eprintln!("Error {:?} executing [{}]", err, install),
+                    )),
+                    Err(err) => log(format!("Error {:?} executing [{}]", err, install)),
                 }
             }
         }
